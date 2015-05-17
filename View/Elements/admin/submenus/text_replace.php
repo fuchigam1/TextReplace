4dfc14be3ddba66765ebc2670075baf3b191a2e1
arata <contact@materializing.net>
2015-05-17 18:11:13 +0900

    fixes サブメニュー追加

--- View/Elements/admin/submenus/text_replace.php
+++ View/Elements/admin/submenus/text_replace.php
@@ -11,8 +11,12 @@
 <tr>
 	<td>
 		<ul>
-			<li><?php $this->BcBaser->link('サブメニュー',
+			<li><?php $this->BcBaser->link('テキスト置換処理',
 					array('plugin' => 'text_replace', 'controller' => 'text_replaces', 'action' => 'index')) ?></li>
+			<li><?php $this->BcBaser->link('サーバーキャッシュ削除',
+					array('admin' => true, 'plugin' => 'null', 'controller' => 'site_configs', 'action' => 'del_cache')) ?></li>
+			<li><?php $this->BcBaser->link('データメンテナンス',
+					array('admin' => true, 'plugin' => 'null', 'controller' => 'tools', 'action' => 'maintenance')) ?></li>
 		</ul>
 	</td>
 </tr>
