80c1e0fb604f7e87015eea9b65aa0f27a47df396
arata <contact@materializing.net>
2015-05-17 16:26:53 +0900

    clear リファクタ

--- Lib/TextReplaceUtil.php
+++ Lib/TextReplaceUtil.php
@@ -7,16 +7,17 @@
  * @package			TextReplace
  * @license			MIT
  */
-class TextReplaceUtil extends Object {
-	
-/**
- * getUseModel
- * 設定ファイルから利用モデルを取得する
- * 
- * @param array $setting
- * @return array
- */
-	public static function getUseModel($setting = array()) {
+class TextReplaceUtil extends Object
+{
+	/**
+	 * getUseModel
+	 * 設定ファイルから利用モデル一覧を取得する
+	 * 
+	 * @param array $setting
+	 * @return array
+	 */
+	public static function getUseModel($setting = array())
+	{
 		$useModel = array();
 		foreach ($setting as $model => $fieldData) {
 			$useModel[] = $model;
@@ -24,14 +25,15 @@ class TextReplaceUtil extends Object {
 		return $useModel;
 	}
 	
-/**
- * getReplaceTarget
- * 設定ファイルから検索置換対象の指定一覧を取得する
- * 
- * @param array $setting
- * @return array
- */
-	public static function getReplaceTarget($setting = array()) {
+	/**
+	 * getReplaceTarget
+	 * 設定ファイルから検索置換対象の指定一覧を取得する
+	 * 
+	 * @param array $setting
+	 * @return array
+	 */
+	public static function getReplaceTarget($setting = array())
+	{
 		$replaceTarget = array();
 		foreach ($setting as $model => $fieldData) {
 			$keyName = $setting[$model]['title'] .'（'. $model. '）';
@@ -40,14 +42,15 @@ class TextReplaceUtil extends Object {
 		return $replaceTarget;
 	}
 	
-/**
- * getUseModelName
- * 設定ファイルから利用モデル名を取得する
- * 
- * @param array $setting
- * @return array
- */
-	public static function getUseModelName($setting = array()) {
+	/**
+	 * getUseModelName
+	 * 設定ファイルから利用モデル名一覧を取得する
+	 * 
+	 * @param array $setting
+	 * @return array
+	 */
+	public static function getUseModelName($setting = array())
+	{
 		$useModel = array();
 		foreach ($setting as $model => $fieldData) {
 			$useModel[] = $fieldData['name'];
@@ -55,17 +58,17 @@ class TextReplaceUtil extends Object {
 		return $useModel;
 	}
 	
-/**
- * getModelField
- * 設定ファイルからモデルとフィールドの対応表を取得する
- * 
- * @param array $setting
- * @return array
- */
-	public static function getModelField($setting = array()) {
+	/**
+	 * getModelField
+	 * 設定ファイルからモデルとフィールドの対応表を取得する
+	 * 
+	 * @param array $setting
+	 * @return array
+	 */
+	public static function getModelField($setting = array())
+	{
 		$data = array();
 		foreach ($setting as $model => $fieldData) {
-			
 			$fieldName = array();
 			foreach ($fieldData['fields'] as $key => $value) {
 				$exploded = explode('.', $key);
