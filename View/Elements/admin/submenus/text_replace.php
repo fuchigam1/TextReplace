153e70e00d31f3bb2200e7620e1c3865e328eebe
arata <contact@materializing.net>
2015-05-17 19:23:59 +0900

    fixes メニュー利用でエラーとなる点を修正

--- View/Elements/admin/submenus/text_replace.php
+++ View/Elements/admin/submenus/text_replace.php
@@ -14,9 +14,9 @@
 			<li><?php $this->BcBaser->link('テキスト置換処理',
 					array('plugin' => 'text_replace', 'controller' => 'text_replaces', 'action' => 'index')) ?></li>
 			<li><?php $this->BcBaser->link('サーバーキャッシュ削除',
-					array('admin' => true, 'plugin' => 'null', 'controller' => 'site_configs', 'action' => 'del_cache')) ?></li>
+					array('admin' => true, 'plugin' => null, 'controller' => 'site_configs', 'action' => 'del_cache')) ?></li>
 			<li><?php $this->BcBaser->link('データメンテナンス',
-					array('admin' => true, 'plugin' => 'null', 'controller' => 'tools', 'action' => 'maintenance')) ?></li>
+					array('admin' => true, 'plugin' => null, 'controller' => 'tools', 'action' => 'maintenance')) ?></li>
 		</ul>
 	</td>
 </tr>
