a86521267884f4429f16f68f258b302114a2d397
arata <contact@materializing.net>
2015-06-07 02:06:03 +0900

    fixes 別画面遷移時、アラートを表示する

--- View/Elements/admin/submenus/text_replace.php
+++ View/Elements/admin/submenus/text_replace.php
@@ -12,11 +12,14 @@
 	<td>
 		<ul>
 			<li><?php $this->BcBaser->link('テキスト置換処理',
-					array('plugin' => 'text_replace', 'controller' => 'text_replaces', 'action' => 'index')) ?></li>
+					array('admin' => true, 'plugin' => 'text_replace', 'controller' => 'text_replaces', 'action' => 'index'),
+					array(), '検索条件は保存されません。よろしいですか？', false); ?></li>
 			<li><?php $this->BcBaser->link('サーバーキャッシュ削除',
-					array('admin' => true, 'plugin' => null, 'controller' => 'site_configs', 'action' => 'del_cache')) ?></li>
+					array('admin' => true, 'plugin' => null, 'controller' => 'site_configs', 'action' => 'del_cache'),
+					array(), '検索条件は保存されません。よろしいですか？', false); ?></li>
 			<li><?php $this->BcBaser->link('データメンテナンス',
-					array('admin' => true, 'plugin' => null, 'controller' => 'tools', 'action' => 'maintenance')) ?></li>
+					array('admin' => true, 'plugin' => null, 'controller' => 'tools', 'action' => 'maintenance'),
+					array(), '検索条件は保存されません。よろしいですか？', false); ?></li>
 		</ul>
 	</td>
 </tr>
