922e0f26eb48c2faa8df4c9db3656a10b8860d51
arata <contact@materializing.net>
2015-08-16 19:18:34 +0900

    fixed js処理をファイル化

--- View/TextReplaces/admin/index.php
+++ View/TextReplaces/admin/index.php
@@ -8,6 +8,8 @@
  * @license			MIT
  */
 $this->BcBaser->css('TextReplace.admin/text_replace', array('inline' => false));
+$this->BcBaser->js(array('TextReplace.admin/text_replace'), array('inline' => false));
+
 $this->TextReplace->searchText = $searchText;
 $this->TextReplace->replaceText = $replaceText;
 
@@ -30,41 +32,10 @@ if ($searchType === 'search-and-replace') {
 	});
 
 	$(function () {
-		// 検索ボタン 実行時
-		$('#BtnTypeSearch').on('click', function(){
-			$('#TextReplaceType').val('search');
-//			if (!confirm('検索置換対象の指定内容で、検索語句を一括検索します。\n宜しいですか？')) {
-//				return false;
-//			}
-		});
 
-		// 置換確認ボタン 実行時
-		$('#BtnTypeDryrun').on('click', function(){
-			$('#TextReplaceType').val('dryrun');
-//			if (!confirm('検索置換対象の指定内容で、検索語句を一括検索し、置換後の状態を表示します。\n宜しいですか？')) {
-//				return false;
-//			}
 		});
-
-		// 置換＆保存ボタン 実行時
-		$('#BtnTypeSearchAndReplace').on('click', function(){
-			$('#TextReplaceType').val('search-and-replace');
-			$('#BtnTypeSearchAndReplaceDialog').dialog({
-				modal: true,
-				title: '置換＆保存',
-				width: 400,
-				buttons: {
-					"キャンセル": function() {
-						$(this).dialog("close");
-					},
-					"OK": function() {
-						$(this).dialog("close");
-						$("#TextReplaceAdminIndexForm").submit();
-					}
 				}
 			});
-			return false;
-		});
 
 		// 検索置換対象のチェックボックスを全てチェックする
 		$('#TextReplaceCheckAll').on('click', function(){
@@ -88,48 +59,6 @@ if ($searchType === 'search-and-replace') {
 			} else {
 				$(this).parent().find('.checkbox input[type=checkbox]').prop('checked', true);
 			}
-		});
-
-		// 検索結果一覧の見方を表示する
-		$('#TextReplaceInsight').hide();
-		$('#helpTextReplaceInsight').on('click', function(){
-			$('#TextReplaceInsight').slideToggle();
-		});
-
-		// モデル別の検索結果を開閉する
-		$('.box-model-result h3').on('click', function(){
-			console.log($(this).next());
-			$(this).next().slideToggle();
-		});
-
-		// 置換対象指定チェックボックスを全てチェックする
-		if ($('#TextReplaceCheckBoxModelResult').prop('checked')) {
-			$('.box-model-result input[type=checkbox]').prop('checked', true);
-		}
-		$('#TextReplaceCheckBoxModelResult').on('click', function(){
-			if ($(this).prop('checked')) {
-				$('.box-model-result input[type=checkbox]').prop('checked', true);
-			} else {
-				$('.box-model-result input[type=checkbox]').prop('checked', false);
-			}
-		});
-
-		// モデル別の検索結果数を表示する
-		$('.box-field-result-all').each(function(){
-			var count = $(this).find('.field-count').html();
-			$(this).parent().children('h3').append(count + '件');
-		});
-
-		// 検索フィールドの結果テキストの検索語句にマークを付ける
-		$('.box-field-result-all').each(function(){
-			$(this).find('.box-field-result').find('.replace-before').each(function(){
-				var text = $(this).html();
-				var patternVal = $('#TextReplaceSearchPattern').val();
-				//console.log(patternVal);
-				var pattern = new RegExp(patternVal);
-				//console.log(pattern);
-				//$(this).html(text.replace(pattern, "<strong>$1</strong>"));
-			});
 		});
 
 	});
---
+++ webroot/js/admin/text_replace.js
@@ -0,0 +1,95 @@
+/**
+ * [ADMIN] OptionalLink
+ *
+ * @link			http://www.materializing.net/
+ * @author			arata
+ * @package			TextReplace
+ * @license			MIT
+ */
+$(function () {
+
+	/**
+	 * 検索・置換ボタン実行時の操作
+	 */
+	// 検索ボタン 実行時
+	$('#BtnTypeSearch').on('click', function(){
+		$('#TextReplaceType').val('search');
+//			if (!confirm('検索置換対象の指定内容で、検索語句を一括検索します。\n宜しいですか？')) {
+//				return false;
+//			}
+	});
+
+	// 置換確認ボタン 実行時
+	$('#BtnTypeDryrun').on('click', function(){
+		$('#TextReplaceType').val('dryrun');
+//			if (!confirm('検索置換対象の指定内容で、検索語句を一括検索し、置換後の状態を表示します。\n宜しいですか？')) {
+//				return false;
+//			}
+	});
+
+	// 置換＆保存ボタン 実行時
+	$('#BtnTypeSearchAndReplace').on('click', function(){
+		$('#TextReplaceType').val('search-and-replace');
+		$('#BtnTypeSearchAndReplaceDialog').dialog({
+			modal: true,
+			title: '置換＆保存',
+			width: 400,
+			buttons: {
+				"キャンセル": function() {
+					$(this).dialog("close");
+				},
+				"OK": function() {
+					$(this).dialog("close");
+					$("#TextReplaceAdminIndexForm").submit();
+				}
+			}
+		});
+		return false;
+	});
+
+	/**
+	 * 検索・置換一覧を操作する
+	 */
+	// 検索結果一覧の見方を表示する
+	$('#TextReplaceInsight').hide();
+	$('#helpTextReplaceInsight').on('click', function(){
+		$('#TextReplaceInsight').slideToggle();
+	});
+
+	// モデル別の検索結果を開閉する
+	$('.box-model-result h3').on('click', function(){
+		console.log($(this).next());
+		$(this).next().slideToggle();
+	});
+
+	// 置換対象指定チェックボックスを全てチェックする
+	if ($('#TextReplaceCheckBoxModelResult').prop('checked')) {
+		$('.box-model-result input[type=checkbox]').prop('checked', true);
+	}
+	$('#TextReplaceCheckBoxModelResult').on('click', function(){
+		if ($(this).prop('checked')) {
+			$('.box-model-result input[type=checkbox]').prop('checked', true);
+		} else {
+			$('.box-model-result input[type=checkbox]').prop('checked', false);
+		}
+	});
+
+	// モデル別の検索結果数を表示する
+	$('.box-field-result-all').each(function(){
+		var count = $(this).find('.field-count').html();
+		$(this).parent().children('h3').append(count + '件');
+	});
+
+	// 検索フィールドの結果テキストの検索語句にマークを付ける
+	$('.box-field-result-all').each(function(){
+		$(this).find('.box-field-result').find('.replace-before').each(function(){
+			var text = $(this).html();
+			var patternVal = $('#TextReplaceSearchPattern').val();
+			//console.log(patternVal);
+			var pattern = new RegExp(patternVal);
+			//console.log(pattern);
+			//$(this).html(text.replace(pattern, "<strong>$1</strong>"));
+		});
+	});
+
+});
