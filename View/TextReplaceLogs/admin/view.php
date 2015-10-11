562041118d583c3f3e46ac8d0fe0d571c0a7db68
arata <contact@materializing.net>
2015-10-11 17:02:21 +0900

    fixes ログ詳細表示のHTMLを調整

--- View/TextReplaceLogs/admin/view.php
+++ View/TextReplaceLogs/admin/view.php
@@ -11,19 +11,19 @@ $this->BcBaser->css('TextReplace.admin/text_replace', array('inline' => false));
 ?>
 <table cellpadding="0" cellspacing="0" class="form-table" id="ListTable">
 	<tr>
-		<th>NO</th>
-		<td><?php echo $data['TextReplaceLog']['id'] ?></td>
-		<th>実施日時（実施ユーザー）</th>
-		<td>
+		<th class="col-head">NO</th>
+		<td class="col-input"><?php echo $data['TextReplaceLog']['id'] ?></td>
+		<th class="col-head">実施日時（実施ユーザー）</th>
+		<td class="col-input">
 			<?php echo $this->BcTime->format('Y/m/d H:i:s', $data['TextReplaceLog']['created']) ?>
 			（<?php echo $userList[$data['TextReplaceLog']['user_id']] ?>）
 		</td>
 	</tr>
 	<tr>
-		<th>検索置換時の対象モデル名</th>
-		<td><?php echo $data['TextReplaceLog']['model'] ?></td>
-		<th>検索置換時の対象フィールド名</th>
-		<td>
+		<th class="col-head">検索置換時の対象モデル名</th>
+		<td class="col-input"><?php echo $data['TextReplaceLog']['model'] ?></td>
+		<th class="col-head">検索置換時の対象フィールド名</th>
+		<td class="col-input">
 			<?php echo $data['TextReplaceLog']['target_field'] ?>
 			&nbsp;&nbsp;&nbsp;&nbsp;
 			<?php $editLink = $this->TextReplace->getEditUrl($data['TextReplaceLog']['model'], $originalData) ?>
@@ -33,19 +33,19 @@ $this->BcBaser->css('TextReplace.admin/text_replace', array('inline' => false));
 		</td>
 	</tr>
 	<tr>
-		<th>検索語句
+		<th class="col-head">検索語句
 			<small>
 			&nbsp;&nbsp;&nbsp;&nbsp;正規表現の利用
 			（<?php echo $this->BcText->booleanMark($data['TextReplaceLog']['search_regex']); ?>）
 			</small>
 		</th>
-		<td colspan="3">
+		<td class="col-input" colspan="3">
 			<?php echo $data['TextReplaceLog']['search_pattern'] ?>
 		</td>
 	</tr>
 	<tr>
-		<th>置換後</th>
-		<td colspan="3">
+		<th class="col-head">置換後</th>
+		<td class="col-input" colspan="3">
 			<?php echo $data['TextReplaceLog']['replace_pattern'] ?>
 		</td>
 	</tr>
@@ -54,17 +54,17 @@ $this->BcBaser->css('TextReplace.admin/text_replace', array('inline' => false));
 <div class="box-field-result">
 <table cellpadding="0" cellspacing="0" class="form-table">
 	<tr>
-		<th>検索置換前の内容</th>
-		<th>検索置換後の内容</th>
+		<th class="col-head">検索置換前の内容</th>
+		<th class="col-head">検索置換後の内容</th>
 	</tr>
 	<tr>
-		<td class="replace-before">
+		<td class="col-input" class="replace-before">
 			<?php echo $this->TextReplace->getBeforeSearchReplaceData(
 					$data['TextReplaceLog']['before_contents'],
 					$data['TextReplaceLog']['search_pattern'],
 					$data['TextReplaceLog']['search_regex']) ?>
 		</td>
-		<td class="replace-after">
+		<td class="col-input" class="replace-after">
 			<?php echo $this->BcBaser->mark($data['TextReplaceLog']['replace_pattern'], nl2br(h($data['TextReplaceLog']['after_contents']))) ?>
 		</td>
 	</tr>
