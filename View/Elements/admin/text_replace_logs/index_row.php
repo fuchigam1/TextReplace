cbe7ba545065f35eccaf46f47b85f58e86cf860d
arata <contact@materializing.net>
2020-01-29 17:15:19 +0900

    fix ボタンが小さく表示されてしまうため

--- View/Elements/admin/text_replace_logs/index_row.php
+++ View/Elements/admin/text_replace_logs/index_row.php
@@ -13,9 +13,9 @@
 		<?php if ($this->BcBaser->isAdminUser()): ?>
 			<?php echo $this->BcForm->checkbox('ListTool.batch_targets.' . $data['TextReplaceLog']['id'], array('type' => 'checkbox', 'class' => 'batch-targets', 'value' => $data['TextReplaceLog']['id'])) ?>
 		<?php endif ?>
-		<?php $this->BcBaser->link($this->BcBaser->getImg('admin/icn_tool_view.png', array('width' => 24, 'height' => 24, 'alt' => '確認', 'class' => 'btn')), array('action' => 'view', $data['TextReplaceLog']['id']), array('title' => '確認')) ?>
+		<?php $this->BcBaser->link($this->BcBaser->getImg('admin/icn_tool_view.png', array('alt' => '確認', 'class' => 'btn')), array('action' => 'view', $data['TextReplaceLog']['id']), array('title' => '確認')) ?>
 		<?php if (BcUtil::isAdminUser()): ?>
-		<?php $this->BcBaser->link($this->BcBaser->getImg('admin/icn_tool_delete.png', array('width' => 24, 'height' => 24, 'alt' => '削除', 'class' => 'btn')),
+		<?php $this->BcBaser->link($this->BcBaser->getImg('admin/icn_tool_delete.png', array('alt' => '削除', 'class' => 'btn')),
 			array('action' => 'ajax_delete', $data['TextReplaceLog']['id']), array('title' => '削除', 'class' => 'btn-delete')) ?>
 		<?php endif ?>
 		</td>
