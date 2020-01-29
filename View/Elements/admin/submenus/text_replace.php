92b961285939395256045b2aece86d6abc050b5f
arata <contact@materializing.net>
2020-01-29 17:15:36 +0900

    fix コア側のユーティリティから実行できるメニューは載せないため

--- View/Elements/admin/submenus/text_replace.php
+++ View/Elements/admin/submenus/text_replace.php
@@ -27,18 +27,6 @@
 				<?php $this->BcBaser->link('<li>テキスト置換ログCSVダウンロード</li>',
 					array('plugin' => 'text_replace', 'controller' => 'text_replace_logs', 'action' => 'download_csv')) ?>
 			<?php endif ?>
-
-			<?php if ($this->request->params['controller'] == 'text_replaces'): ?>
-				<?php $this->BcBaser->link('<li>固定ページテンプレート書出</li>',
-						array('admin' => true, 'plugin' => null, 'controller' => 'pages', 'action' => 'write_page_files'),
-						array('confirm' => 'データベース内のページデータを、' . 'テーマ ' . Inflector::camelize($this->BcBaser->siteConfig['theme']) . " のページテンプレートとして全て書出します。\n本当によろしいですか？")) ?>
-				<?php $this->BcBaser->link('<li>サーバーキャッシュ削除</li>',
-						array('admin' => true, 'plugin' => null, 'controller' => 'site_configs', 'action' => 'del_cache'),
-						array(), '検索条件は保存されません。よろしいですか？', false); ?>
-				<?php $this->BcBaser->link('<li>データメンテナンス</li>',
-						array('admin' => true, 'plugin' => null, 'controller' => 'tools', 'action' => 'maintenance'),
-						array(), '検索条件は保存されません。よろしいですか？', false); ?>
-			<?php endif ?>
 		</ul>
 	</td>
 </tr>
