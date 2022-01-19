efaaa90192330400601a317571ead2403f670869
arata <contact@materializing.net>
2022-01-19 15:24:37 +0900

    fix admin-third対応: ログ一覧画面調整

--- View/Elements/admin/text_replace_logs/index_list.php
+++ View/Elements/admin/text_replace_logs/index_list.php
@@ -7,7 +7,112 @@
  * @package TextReplace
  * @license MIT
  */
+$this->BcListTable->setColumnNumber(8);
 ?>
+<?php if ($siteConfig['admin_theme']): ?>
+<div class="bca-data-list__top">
+	<!-- 一括処理 -->
+	<?php if ($this->BcBaser->isAdminUser()): ?>
+		<div class="bca-action-table-listup">
+				<?php echo $this->BcForm->input('ListTool.batch', array('type' => 'select', 'options' => array('del' => '削除'), 'empty' => '一括処理')) ?>
+				<?php echo $this->BcForm->button('適用', array('id' => 'BtnApplyBatch', 'disabled' => 'disabled')) ?>
+		</div>
+	<?php endif ?>
+	<div class="bca-data-list__sub">
+		<!-- pagination -->
+		<?php $this->BcBaser->element('pagination') ?>
+	</div>
+</div>
+
+
+<table class="list-table bca-table-listup" id="ListTable">
+	<thead class="bca-table-listup__thead">
+		<tr>
+			<th class="list-tool bca-table-listup__thead-th bca-table-listup__thead-th--select" title="<?php echo __d('baser', '一括選択') ?>">
+				<?php if ($this->BcBaser->isAdminUser()): ?>
+					<?php echo $this->BcForm->input('ListTool.checkall', ['type' => 'checkbox', 'label' => ' ', 'title' => __d('baser', '一括選択')]) ?>
+				<?php endif; ?>
+			</th>
+			<th class="bca-table-listup__thead-th">
+				<?php echo $this->Paginator->sort('id',
+					[
+						'asc' => '<i class="bca-icon--asc"></i>' . __d('baser', 'No'),
+						'desc' => '<i class="bca-icon--desc"></i>' . __d('baser', 'No')
+					],
+					['escape' => false, 'class' => 'btn-direction bca-table-listup__a']
+				) ?>
+			</th>
+			<th class="bca-table-listup__thead-th">
+				<?php echo $this->Paginator->sort('model_id',
+					[
+						'asc' => '<i class="bca-icon--asc"></i>' . __d('baser', 'モデルID'),
+						'desc' => '<i class="bca-icon--desc"></i>' . __d('baser', 'モデルID')
+					],
+					['escape' => false, 'class' => 'btn-direction bca-table-listup__a']
+				) ?>
+			</th>
+			<th class="bca-table-listup__thead-th">
+				<?php echo $this->Paginator->sort('model',
+					[
+						'asc' => '<i class="bca-icon--asc"></i>' . __d('baser', 'モデル名'),
+						'desc' => '<i class="bca-icon--desc"></i>' . __d('baser', 'モデル名')
+					],
+					['escape' => false, 'class' => 'btn-direction bca-table-listup__a']
+				) ?>
+				<br />
+				<?php echo $this->Paginator->sort('target_field',
+					[
+						'asc' => '<i class="bca-icon--asc"></i>' . __d('baser', '対象フィールド名'),
+						'desc' => '<i class="bca-icon--desc"></i>' . __d('baser', '対象フィールド名')
+					],
+					['escape' => false, 'class' => 'btn-direction bca-table-listup__a']
+				) ?>
+			</th>
+			<th class="bca-table-listup__thead-th">検索置換前の内容</th>
+			<th class="bca-table-listup__thead-th">検索置換後の内容</th>
+			<th class="bca-table-listup__thead-th">
+				<?php echo $this->Paginator->sort('created',
+					[
+						'asc' => '<i class="bca-icon--asc"></i>' . __d('baser', '実施日時'),
+						'desc' => '<i class="bca-icon--desc"></i>' . __d('baser', '実施日時')
+					],
+					['escape' => false, 'class' => 'btn-direction bca-table-listup__a']
+				) ?>
+			</th>
+			<th class="bca-table-listup__thead-th"><?php // アクション ?>
+				<?php echo __d('baser', 'アクション') ?>
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php if(!empty($datas)): ?>
+			<?php foreach($datas as $data): ?>
+				<?php $this->BcBaser->element('text_replace_logs/index_row', array('data' => $data)) ?>
+			<?php endforeach; ?>
+		<?php else: ?>
+			<tr>
+				<td colspan="<?php echo $this->BcListTable->getColumnNumber() ?>" class="bca-table-listup__tbody-td">
+					<p class="no-data"><?php echo __d('baser', 'データがありません。') ?></p>
+				</td>
+			</tr>
+		<?php endif; ?>
+	</tbody>
+</table>
+
+<div class="bca-data-list__bottom">
+	<div class="bca-data-list__sub">
+		<!-- pagination -->
+		<?php $this->BcBaser->element('pagination') ?>
+		<!-- list-num -->
+		<?php $this->BcBaser->element('list_num') ?>
+	</div>
+</div>
+
+<?php return; ?>
+<?php endif; ?>
+<? // admin-secondは以下 ?>
+
+
 <?php $this->BcBaser->element('pagination') ?>
 
 <table cellpadding="0" cellspacing="0" class="list-table sort-table" id="ListTable">
--- View/Elements/admin/text_replace_logs/index_row.php
+++ View/Elements/admin/text_replace_logs/index_row.php
@@ -8,6 +8,45 @@
  * @license MIT
  */
 ?>
+<?php if ($siteConfig['admin_theme']): ?>
+<tr>
+	<td class="bca-table-listup__tbody-td bca-table-listup__tbody-td--select"><?php // 選択 ?>
+		<?php if ($this->BcBaser->isAdminUser()): ?>
+			<?php echo $this->BcForm->checkbox('ListTool.batch_targets.' . $data['TextReplaceLog']['id'], array('type' => 'checkbox', 'class' => 'batch-targets bca-checkbox__input', 'value' => $data['TextReplaceLog']['id'], 'label' => '<span class="bca-visually-hidden">' . __d('baser', 'チェックする') . '</span>')) ?>
+		<?php endif ?>
+	</td>
+	<td class="bca-table-listup__tbody-td">
+		<?php echo $this->BcBaser->link($data['TextReplaceLog']['id'], array('action' => 'view', $data['TextReplaceLog']['id']), array('title' => '確認')) ?>
+	</td>
+	<td class="bca-table-listup__tbody-td">
+		<?php echo $data['TextReplaceLog']['model_id'] ?>
+	</td>
+	<td class="bca-table-listup__tbody-td">
+		<?php echo $data['TextReplaceLog']['model'] ?><br />
+		<?php echo $data['TextReplaceLog']['target_field'] ?>
+	</td>
+	<td class="bca-table-listup__tbody-td">
+		<?php echo $this->Text->truncate(h($data['TextReplaceLog']['before_contents']), 70) ?>
+	</td>
+	<td class="bca-table-listup__tbody-td">
+		<?php echo $this->Text->truncate(h($data['TextReplaceLog']['after_contents']), 70) ?>
+	</td>
+	<td class="bca-table-listup__tbody-td" style="white-space: nowrap">
+		<?php echo $this->BcTime->format('Y-m-d', $data['TextReplaceLog']['created']) ?>
+		<br />
+		<?php echo $this->BcTime->format('H:i:s', $data['TextReplaceLog']['created']) ?>
+	</td>
+	<td class="row-tools bca-table-listup__tbody-td bca-table-listup__tbody-td--actions"><?php // アクション ?>
+		<?php $this->BcBaser->link('', ['action' => 'view', $data['TextReplaceLog']['id']], ['title' => __d('baser', '確認'), 'class' => 'bca-btn-icon', 'data-bca-btn-type' => 'preview', 'data-bca-btn-size' => 'lg']) ?>
+		<?php if (BcUtil::isAdminUser()): ?>
+			<?php $this->BcBaser->link('', ['action' => 'ajax_delete', $data['TextReplaceLog']['id']], ['title' => __d('baser', '削除'), 'class' => 'btn-delete bca-btn-icon', 'data-bca-btn-type' => 'delete', 'data-bca-btn-size' => 'lg']) ?>
+		<?php endif ?>
+	</td>
+</tr>
+<?php return; ?>
+<?php endif; ?>
+<? // admin-secondは以下 ?>
+
 <tr>
 	<td class="row-tools">
 		<?php if ($this->BcBaser->isAdminUser()): ?>
